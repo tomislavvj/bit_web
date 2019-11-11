50587f171fbccc6f20e0db0c2dc3f3340c10ca39
unknown <tomislavvj@gmail.com>
2019-11-12 00:36:40 +0100

    MARIO add

--- mario/script.js
+++ mario/script.js
@@ -1,11 +1,44 @@
 var body = document.body;
 var marioStatic = document.getElementById("marioStatic");
 var marioActive = document.getElementById("marioActive");
+var marioActiveLeft=document.getElementById("marioActiveLeft");
 
-var event = document.addEventListener("click", moveRight);
+/* var event = body.addEventListener("click", runMario);
+ */
+function RunMarioLeft(){
+  marioStatic.classList.add("hide");
+  marioActiveLeft.classList.remove("hide");
 
-function moveRight(event) {
-  marioStatic.className = "";
+}
+
+function runMario() {
+  marioStatic.classList.add("hide");
   marioActive.classList.remove("hide");
 }
-moveRight();
+
+
+function startPosition() {
+  marioStatic.classList.remove("hide");
+  marioActive.classList.add("hide")
+}
+
+
+
+/* document.onkeydown = checkKey;
+ */
+document.onkeyup = checkKey;
+
+function checkKey(e) {
+  if (e.keyCode == "40") {
+    startPosition();
+  }
+}
+
+function checkKey(e) {
+  if (e.keyCode == "39") {
+    runMario();
+  }
+  else if(e.keyCode=="37"){
+    RunMarioLeft();
+  }
+}
