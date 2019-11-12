4630296886999275c47e3399cc03b91f03318d2d
BIT Student <classroom@bgit.rs>
2019-11-12 17:42:52 +0100

    SUPER MARIO DONE

--- mario/script.js
+++ mario/script.js
@@ -1,44 +1,59 @@
 var body = document.body;
-var marioStatic = document.getElementById("marioStatic");
+var marioStatic = document.querySelector(".marioStatic");
 var marioActive = document.getElementById("marioActive");
-var marioActiveLeft=document.getElementById("marioActiveLeft");
+var marioActiveLeft = document.getElementById("marioActiveLeft");
 
-/* var event = body.addEventListener("click", runMario);
- */
-function RunMarioLeft(){
+function RunMarioLeft() {
   marioStatic.classList.add("hide");
   marioActiveLeft.classList.remove("hide");
-
+  marioActive.classList.add("hide");
+  marioActiveLeft.classList.add("reverse");
+  body.classList.add("animationClass");
 }
 
 function runMario() {
   marioStatic.classList.add("hide");
   marioActive.classList.remove("hide");
+  marioActiveLeft.classList.add("hide");
+  body.classList.add("animationClassReverse");
 }
-
 
 function startPosition() {
   marioStatic.classList.remove("hide");
-  marioActive.classList.add("hide")
+  marioActive.classList.add("hide");
+  marioActiveLeft.classList.add("hide");
+  body.classList.remove("animationClass");
+  body.classList.remove("animationClassReverse");
+  marioStatic.classList.remove("marioJump");
 }
 
-
-
-/* document.onkeydown = checkKey;
- */
-document.onkeyup = checkKey;
-
-function checkKey(e) {
-  if (e.keyCode == "40") {
-    startPosition();
-  }
+function jumpMario() {
+  marioStatic.classList.remove("hide");
+  marioActive.classList.add("hide");
+  marioActiveLeft.classList.add("hide");
+  marioStatic.classList.add("marioJump");
 }
+
+onkeydown = checkKey;
 
 function checkKey(e) {
   if (e.keyCode == "39") {
     runMario();
-  }
-  else if(e.keyCode=="37"){
+  } else if (e.keyCode == "37") {
     RunMarioLeft();
+  } else if (e.keyCode == "32") {
+    jumpMario();
+  }
+}
+
+onkeyup = stopKey;
+
+function stopKey(event) {
+  if (event.keyCode == "39") {
+    startPosition();
+  } else if (event.keyCode == "37") {
+    startPosition();
+  } else if (event.keyCode == "32") {
+    startPosition();
   }
 }
